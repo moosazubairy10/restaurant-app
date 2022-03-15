6353a63a16a8ebbb9bae41cbeb4e45ba8bba3aa2
Moosa <moosazubairy@gmail.com>
2022-03-16 03:26:03 +0500

    restaurantproducts updated

--- src/components/main/restaurant-product/RestaurantProductMenuSection.jsx
+++ src/components/main/restaurant-product/RestaurantProductMenuSection.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import "../../../styles/main/restaurant-product/restaurant_product_menu_section.css";
 import RestaurantProductMenu from "./RestaurantProductMenu";
 import RestaurantProductOrder from "./RestaurantProductOrder";
+import "../../homeComponents/first-section.css";
 const RestaurantProductMenuSection = () => {
   return (
-    <div className="restaurant_product_menu_section">
-      <div className="container-fluid restaurant_product_menu_section_container">
-        <div className="restaurant_product_menu_section_container_menuList">
+    <div className="topContainer" style={{
+      display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', width: '100%'
+    }}>
+      <div className="columns" style={{ width: '96%' }}>
+        <div className="column" style={{ width: '67%' }}>
           <RestaurantProductMenu />
         </div>
-        <div className="restaurant_product_menu_section_container_order">
+        <div className="column" style={{ width: '33%' }} >
           <RestaurantProductOrder />
         </div>
       </div>
--- src/components/main/restaurant-product/RestaurantProductSlider.jsx
+++ src/components/main/restaurant-product/RestaurantProductSlider.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "../../../styles/main/restaurant-product/restaurant-product-slider.css";
+import "../../homeComponents/first-section.css"
 const RestaurantProductSlider = () => {
   return (
     <div className="container-fluid restaurant_product_slider_container">
+
       <Swiper
+        className="column"
         // install Swiper modules
         modules={[Navigation, A11y]}
         spaceBetween={50}
-        slidesPerView={7}
+        slidesPerView={5}
         navigation
+        SwiperSlide={{ outerWidth: '150px' }}
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         onSwiper={(swiper) => console.log(swiper)}
@@ -35,6 +38,8 @@ const RestaurantProductSlider = () => {
 
       {/* </div> */}
     </div>
+
+
   );
 };
 
